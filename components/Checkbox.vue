26040278483403d8fd35bd36ef5ca66c9481ab0a
Topo1993 <jorgeou1993@gmail.com>
2020-09-26 03:46:03 -0500

    cleaned Checkbox component

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -1,25 +1,34 @@
 <template>
   <div
-    class="p-3 lg:p-4 lg:text-lg border text-sm lg:px-8 px-6 mr-3 mb-3 border-gray-300 transition-all hover:border-gray-900 cursor-pointer duration-500 rounded-full"
-    :class="{ active: hover }"
-    @click="hover = !hover"
+    class="p-3 lg:p-4 lg:text-lg border text-sm lg:px-8 px-6 mr-3 mb-3 border-gray-300 transition-all hover:border-gray-900 cursor-pointer duration-500 rounded-full text-gray-900"
+    :class="{ active: checked }"
+    @click="toggleChecked"
   >
-    <p class="font-bold">{{ text }}</p>
+    <p class="font-bold">{{ name }}</p>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    text: {
+    name: {
       type: String,
-      default: 'Text',
+      default: 'Name',
+    },
+    checked: {
+      type: Boolean,
+      default: false,
     },
   },
-  data() {
-    return {
-      hover: false,
-    }
+  methods: {
+    toggleChecked() {
+      this.$emit(
+        'toggle:checked',
+        { service_name: this.name },
+        { last_checked_state: this.checked },
+        { now_checked_state: !this.checked }
+      )
+    },
   },
 }
 </script>
@@ -33,5 +42,4 @@ export default {
 .active
   background: #111111
   color: white
-  // border: solid 2px #111111
 </style>
